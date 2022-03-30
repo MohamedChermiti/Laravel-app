f86e16470eabe1b46bbda3ab9b979527132cea0e
MohamedChermiti <mohamed.chermiti@sesame.com.tn>
2022-03-30 16:57:08 +0100

    third-commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,11 +1,34 @@
 <?php
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Prophecy\Promise\ReturnPromise;
 
-class Post 
-{   
+class Post
+{
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+    /**
+     * @param $title
+     * @param $excerpt
+     * @param $date
+     * @param $body
+     */
+    public function __construct($title, $excerpt, $date, $body,$slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+
     public static function all() {
         $files = File::files(resource_path("posts/"));
         return array_map(function($file) {
@@ -14,13 +37,16 @@ class Post
     }
 
 
+    /**
+     * @throws Exception
+     */
     public static function find ($slug) {
         if (! file_exists($path = resource_path("posts/{$slug}.html"))){
             //return redirect('/posts');
             throw new ModelNotFoundException();
         }
-        return cache()->remember("posts.{$slug}", now()->addDay(), fn() =>file_get_contents($path)); 
+        return cache()->remember("posts.{$slug}", now()->addDay(), fn() =>file_get_contents($path));
             //return file_get_contents($path);
     }
-    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,53 @@
 <?php
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
- //med
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('/posts', function () {
+    $files = File::files(resource_path("posts"));
+    collect()
+    $posts = [];
+    foreach ($files as $file){
+        $document = YamlFrontMatter::parseFile($file);
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }
+
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
+    ]);
+});*/
+
+//This is another implementation of the route /posts using array_map
+Route::get('/posts', function () {
+    $posts = collect(File::files(resource_path("posts")))
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn($document) => new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        ));
+
+    return view('posts', [
+        'posts' => $posts
     ]);
 });
 
-Route::get('/posts/{post}', function ($slug) {
-    $document = YamlFrontMatter::parseFile(
 
-    );
+Route::get('/posts/{post}', function ($slug) {
 
     return view('post', [
         'post' => Post::find($slug)
